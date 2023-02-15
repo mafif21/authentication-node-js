9e7a310834d01198dfb5e41403ea77592e7a1fd0
unknown <afif.maliki21@gmail.com>
2023-02-15 11:25:28 +0700

    update gitignore

--- model/User.js
+++ model/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const url = "mongodb://localhost:27017/secretapp";
 const encrypt = require("mongoose-field-encryption").fieldEncryption;
+require("dotenv").config();
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
@@ -15,7 +16,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.plugin(encrypt, {
   fields: ["password"],
-  secret: "randomstringforscretkey",
+  secret: process.env.SECRET_KEY,
 });
 
 const User = mongoose.model("user", userSchema);
